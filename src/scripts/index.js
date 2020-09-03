eee697404c624ada6bfc78fd2208c17bc158ab2e
utrodus <come.programmer@gmail.com>
2020-09-03 12:56:08 +0700

    Navbar (main navigation) selesai

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,6 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.scss';
+import NavConfig from './NavConfig.js';
 
-console.log('Hello Coders! :)');
+
+document.addEventListener("DOMContentLoaded", NavConfig);
