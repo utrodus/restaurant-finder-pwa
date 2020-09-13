47a78b6effef00ac4574cd578c247e667f418d86
utrodus <come.programmer@gmail.com>
2020-09-13 20:18:20 +0700

    remove unused file and renaming hero img

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,7 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.scss';
 import App from './App.js';
-
+import '../public/svg/logo.svg';
+import '../public/svg/featured-img.svg';
 
 document.addEventListener("DOMContentLoaded", App);
