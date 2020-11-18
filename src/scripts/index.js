35b44099e70d5b8a8cba61732b1ce06c70e76d75
utrodus <come.programmer@gmail.com>
2020-11-18 17:06:34 +0700

    Implement app-header component

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,7 @@
 import "regenerator-runtime"; /* for async await transpile */
 import "../styles/index.scss";
 import App from "./views/app";
+import "./components/header";
 
 const app = new App({
   button: document.querySelector("#menu__button"),
