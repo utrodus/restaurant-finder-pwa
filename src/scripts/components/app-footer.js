c8e0ef97a838f174e6f07cb697b6587661673304
utrodus <come.programmer@gmail.com>
2020-11-18 17:08:52 +0700

    rename file app-header component & create footer component

---
+++ src/scripts/components/app-footer.js
@@ -0,0 +1,15 @@
+class AppFooter extends HTMLElement {
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    this.innerHTML = `
+      <footer>
+      <p class="footer__copyright">Copyright © 2020 CariResto</p>
+    </footer>
+      `;
+  }
+}
+
+customElements.define("app-footer", AppFooter);
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,8 @@
 import "regenerator-runtime"; /* for async await transpile */
 import "../styles/index.scss";
 import App from "./views/app";
-import "./components/header";
+import "./components/app-header";
+import "./components/app-footer";
 
 const app = new App({
   button: document.querySelector("#menu__button"),
