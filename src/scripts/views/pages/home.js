21da926220e0e6f935986ffdbe33217abf11f9d8
utrodus <come.programmer@gmail.com>
2020-11-18 11:13:19 +0700

    fix import restaurantItemTemplate

--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -1,5 +1,5 @@
 import RestaurantsDbSource from "../../data/restaurantsdb-source";
-import createRestaurantItemTemplate from "../templates/template-creator";
+import {createRestaurantItemTemplate} from "../templates/template-creator";
 
 const Home = {
   async render() {
