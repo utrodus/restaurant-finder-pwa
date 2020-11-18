f9cd5ca5a3aef84010e1ed3ce8da99cb2e4b4963
utrodus <come.programmer@gmail.com>
2020-11-18 17:06:00 +0700

    create custom element app header

---
+++ src/scripts/components/header.js
@@ -0,0 +1,37 @@
+class AppHeader extends HTMLElement {
+    connectedCallback() {
+      this.render();
+    }
+  
+    render() {
+      this.innerHTML = `
+      <header>
+      <div class="header__content">
+        <div class="app__bar">
+          <a id="menu__button" aria-label="navigation-menu" tabindex="0">☰</a>
+          <a href="/" class="logo" tabindex="0">
+            <img src=" ./svg/logo.svg" alt="Logo Cari Resto"
+          /></a>
+        </div>
+        <nav id="app__menu">
+          <ul>
+            <li><a href="#/home" class="active" tabindex="0">Home</a></li>
+            <li><a href="#/favorites" tabindex="0">Favorites</a></li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/utrodus-said/"
+                target="_blank"
+                tabindex="0"
+                >About Us</a
+              >
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+      `;
+    }
+  }
+  
+  customElements.define('app-header', AppHeader);
+  
