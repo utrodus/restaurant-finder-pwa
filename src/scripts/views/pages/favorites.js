c820624c27aebf0f8f640f6ce81d680ab837f8ae
utrodus <come.programmer@gmail.com>
2020-11-18 22:08:09 +0700

    change id favorite page to favoritesRestaurant

--- src/scripts/views/pages/favorites.js
+++ src/scripts/views/pages/favorites.js
@@ -1,11 +1,14 @@
 import RestaurantsDbSource from "../../data/restaurantsdb-source";
-import {createRestaurantItemTemplate} from "../templates/template-creator";
+import {
+  createRestaurantItemTemplate,
+  createEmptyFavoritePageTemplate,
+} from "../templates/template-creator";
 const Favorites = {
   async render() {
     return `
       <section class="content">
-        <article id="restaurants" class="restaurants">
-          <div class="container">
+        <article id="favoritesRestaurant" class="favorites_restaurant">
+          <div class="container" id="favoritesContainer">
             <h1>Favorites</h1>
             <hr class="separator" />
             <p>
@@ -19,17 +22,22 @@ const Favorites = {
   },
 
   async afterRender() {
-    // Fungsi ini akan dipanggil setelah render()
-    // Fungsi ini akan dipanggil setelah render()
     const restaurants = await RestaurantsDbSource.restaurantsList();
     const restaurantListContainer = document.getElementById(
       "restaurants__list"
     );
-    restaurants.forEach((restaurant) => {
-      restaurantListContainer.innerHTML += createRestaurantItemTemplate(
-        restaurant
-      );
-    });
+
+    const favoritesPageContainer = document.getElementById(
+      "favoritesContainer"
+    );
+
+    favoritesPageContainer.innerHTML += createEmptyFavoritePageTemplate();
+
+    // restaurants.forEach((restaurant) => {
+    //   restaurantListContainer.innerHTML += createRestaurantItemTemplate(
+    //     restaurant
+    //   );
+    // });
   },
 };
 
