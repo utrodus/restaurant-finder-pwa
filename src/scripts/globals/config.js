72a11ddde3709697e4c23337f5210e2ea49880ac
utrodus <come.programmer@gmail.com>
2020-11-21 17:34:03 +0700

    add config index db & add functionality

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,8 @@
 const CONFIG = {
   BASE_URL: "https://restaurant-api.dicoding.dev",
+  DATABASE_NAME: "restaurants-database",
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: "restaurants",
 };
 
 export default CONFIG;
