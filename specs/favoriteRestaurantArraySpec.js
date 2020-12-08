32a5743da686963ecb0d5ba1c8f970e18cbcb82f
utrodus <come.programmer@gmail.com>
2020-12-08 19:05:52 +0700

    remove unused comment

--- specs/favoriteRestaurantArraySpec.js
+++ specs/favoriteRestaurantArraySpec.js
@@ -20,7 +20,6 @@ const FavoriteRestaurantArray = {
       return;
     }
 
-    // pastikan id ini belum ada dalam daftar favoriteRestaurant
     if (this.getRestaurant(restaurant.id)) {
       return;
     }
@@ -29,8 +28,6 @@ const FavoriteRestaurantArray = {
   },
 
   deleteRestaurant(id) {
-    // cara boros menghapus restaurant dengan meng-copy restaurant yang ada
-    // kecuali restaurant dengan id === id
     favoriteRestaurants = favoriteRestaurants.filter((restaurant) => restaurant.id !== id);
   },
 };
